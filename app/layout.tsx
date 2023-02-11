899557d99ff4d3e3ca1581563e22b04a0a866354
danybeltran <josuebeltran_2010@hotmail.com>
2023-02-10 22:39:40 -0600

    feat: - Adds 'Get the code' button - Adds head description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 import { FetchConfig } from 'http-react'
 import './globals.css'
 import 'bs-icon/icons.css'
+import Link from 'next/link'
+import Icon from 'bs-icon'
 
 function MainLayout({ children }) {
   return (
     <FetchConfig baseUrl='/api'>
-      <html data-theme="corporate">
+      <html data-theme='corporate'>
         <head>
-          <title>Nextjs</title>
+          <title>Next.js starter</title>
+          <meta name='description' content='A Starter with Next.js' />
         </head>
         <body>
           <div className='p-8'>{children}</div>
+          <Link
+            href='https://github.com/danybeltran/nextjs-typescript-and-mongodb'
+            target='_blank'
+            className='fixed bottom-2 left-2 btn btn-sm gap-x-2'
+          >
+            <Icon name='github' /> <span>Get the code</span>
+          </Link>
         </body>
       </html>
     </FetchConfig>
