a8776424d8f66b9b58c9fb0a77b53b8099f158df
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-15 13:04:25 +0600

    Build PostDeleteButton

--- app/posts/_components/PostCard.tsx
+++ app/posts/_components/PostCard.tsx
@@ -8,33 +8,12 @@ import {
   CardHeader,
   CardTitle
 } from '../../../components/ui/card'
-import { Button } from 'components/ui/button'
-import useFetch, { revalidate } from 'http-react'
+import PostDeleteButton from './PostDeleteButton'
 interface Props {
   post: Post
 }
 
-function confirmPostDelete(id: any) {
-  const confirmation = confirm('Do you want to remove this post?')
-  if (confirmation) {
-    revalidate(id)
-  }
-}
-
 const PostCard = ({ post }: Props) => {
-  const fetchID = ['delete', post.id].join('-')
-
-  useFetch('/posts', {
-    id: fetchID,
-    method: 'DELETE',
-    query: {
-      id: post.id
-    },
-    onResolve() {
-      revalidate('GET /posts')
-    }
-  })
-
   return (
     <Card className='h-fit'>
       <CardHeader>
@@ -45,13 +24,7 @@ const PostCard = ({ post }: Props) => {
         <p className='line-clamp-2'>{post.content}</p>
       </CardContent>
       <CardFooter className='flex justify-end'>
-        <Button
-          onClick={() => confirmPostDelete(fetchID)}
-          size='sm'
-          variant='outline'
-        >
-          Delete
-        </Button>
+        <PostDeleteButton postId={post.id} />
       </CardFooter>
     </Card>
   )
