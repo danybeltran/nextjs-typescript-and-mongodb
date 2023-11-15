d418e282419a5bad6eed3c099ad8796f63eadef5
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-15 08:27:56 +0600

    Uninstall daisyui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,28 @@
 import './globals.css'
-import { AtomicState } from 'atomic-state'
 import { FetchConfig } from 'http-react'
 import Link from 'next/link'
 import Icon from 'bs-icon'
-import { cookies } from 'next/headers'
-
-import ThemeToggle from 'components/ThemeToggle'
-import ThemeWrap from 'components/ThemeWrapp'
 
 function MainLayout({ children }) {
-  const theme = JSON.parse(cookies().get('theme')?.value ?? '"dark"')
-
   return (
     <html>
       <head>
         <title>Next.js starter</title>
         <meta name='description' content='A Starter with Next.js' />
       </head>
-      <AtomicState
-        default={{
-          theme
-        }}
-      >
-        <FetchConfig baseUrl='/api'>
-          <body>
-            <ThemeWrap>
-              <ThemeToggle />
-              {children}
-              <Link
-                href='https://github.com/danybeltran/nextjs-typescript-and-mongodb'
-                target='_blank'
-                className='fixed bottom-2 left-2 btn btn-sm gap-x-2'
-              >
-                <Icon name='github' /> <span>Github</span>
-              </Link>
-            </ThemeWrap>
-          </body>
-        </FetchConfig>
-      </AtomicState>
+
+      <FetchConfig baseUrl='/api'>
+        <body>
+          {children}
+          <Link
+            href='https://github.com/danybeltran/nextjs-typescript-and-mongodb'
+            target='_blank'
+            className='fixed bottom-2 left-2 btn btn-sm gap-x-2'
+          >
+            <Icon name='github' /> <span>Github</span>
+          </Link>
+        </body>
+      </FetchConfig>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./app/**/*.tsx', './components/**/*.tsx'],
-  darkMode: false,
-  variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
-    fontSize: ['responsive', 'hover', 'focus', 'active'],
-    padding: ['responsive', 'hover', 'focus', 'active'],
-    margin: ['responsive', 'hover', 'focus', 'active'],
-    align: ['responsive', 'hover', 'focus', 'active']
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {}
   },
-  daisyui: {
-    themes: ['dark', 'light']
-  },
-  plugins: [require('daisyui')]
+  plugins: []
 }
