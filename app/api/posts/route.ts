3b7b4778aab33b297dd2617b2ec46605acba9555
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-16 17:45:07 +0600

    Implement data validation on server

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,13 +1,19 @@
 import { prisma } from 'server'
-import { Types } from 'types'
 import { NextResponse } from 'next/server'
+import { postSchema } from 'app/schemasValidations'
 
-export async function POST(req: Request) {
+export async function POST(request: Request) {
   try {
-    const body: Types.Post = await req.json()
+    const body = await request.json()
+    const validation = postSchema.safeParse(body)
+    if (!validation.success) {
+      return NextResponse.json(validation.error.format(), { status: 400 })
+    }
+
     const newPost = await prisma.post.create({
       data: body
     })
+
     return NextResponse.json(newPost)
   } catch {
     return NextResponse.json('error', {
