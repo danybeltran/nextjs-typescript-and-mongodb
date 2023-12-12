de48b65ce602a1d876fe3e57e003a18bc3e6ba59
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-12-12 08:54:26 +0600

    Clean duplicate code

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,59 +1,8 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { Button } from 'components/ui/button'
-import { Textarea } from 'components/ui/textarea'
-import { Input } from 'components/ui/input'
-import { AlertCircle, ArrowLeft, Loader2 } from 'lucide-react'
-import { postSchema } from 'app/schemasValidations'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from 'components/ui/form'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
-import axios from 'axios'
-import { Alert, AlertTitle } from 'components/ui/alert'
+import { ArrowLeft } from 'lucide-react'
 import PostForm from '../_components/PostForm'
 
-type FormSchema = z.infer<typeof postSchema>
-
 export default function Create() {
-  const router = useRouter()
-
-  const [error, setError] = useState('')
-  const [isSubmitting, setSubmitting] = useState(false)
-
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(postSchema),
-    defaultValues: {
-      title: '',
-      content: ''
-    }
-  })
-
-  const onSubmit = form.handleSubmit(async data => {
-    try {
-      setSubmitting(true)
-      await axios.post('/api/posts', {
-        ...data
-      })
-      router.push('/posts')
-      router.refresh()
-    } catch (error) {
-      console.error(error)
-      setSubmitting(false)
-      setError('An unexpected error occurred.')
-    }
-  })
-
   return (
     <section>
       <Link href='/posts' className='flex gap-1 items-center '>
@@ -64,7 +13,6 @@ export default function Create() {
         <header className='my-4 md:my-8'>
           <h1 className='font-bold text-2xl'>Add Post</h1>
         </header>
-
         <PostForm />
       </div>
     </section>
